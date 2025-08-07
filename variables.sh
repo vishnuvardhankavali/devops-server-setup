1b6ffb582b3c84cf29762db946f185e7bb7ae709
vishnuvardhankavali <kavallivishnuvardhan@gmail.com>
2025-08-07 11:17:42 +0000

    Devops student script

---
+++ variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Ask name 
+echo "Enter you name"
+read name 
+
+echo "Nice to meet you!$name, you want to learn Devops (yes/no)"
+
+read answer
+if [ "$answer" == "yes" ]; then
+echo "Good decision, Keep going, forward beast mode in it"
+else
+echo " Try to start learning"
+
+echo "keep Going $vishnu"
+fi
